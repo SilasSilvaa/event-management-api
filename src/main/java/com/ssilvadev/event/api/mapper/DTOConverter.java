e5d1cbb296a9d89e68397dec092e71556c6539ce
Silassilvaa <silas-oli@hotmail.com>
2025-05-07 22:46:16 -0300

    feat: creating static method to convert ResponseUserDTO to User Entity

--- src/main/java/com/ssilvadev/event/api/mapper/DTOConverter.java
+++ src/main/java/com/ssilvadev/event/api/mapper/DTOConverter.java
@@ -2,6 +2,9 @@ package com.ssilvadev.event.api.mapper;
 
 import com.ssilvadev.event.api.dto.user.response.Gender;
 import com.ssilvadev.event.api.dto.user.response.ResponseUserDTO;
+import com.ssilvadev.event.api.model.user.Email;
+import com.ssilvadev.event.api.model.user.LastName;
+import com.ssilvadev.event.api.model.user.Name;
 import com.ssilvadev.event.api.model.user.User;
 
 public class DTOConverter {
@@ -15,5 +18,19 @@ public class DTOConverter {
                 Gender.valueOf(user.getGender()));
 
         return dto;
+    }
+
+    public static User responseDTOToUser(ResponseUserDTO user) {
+        var name = new Name(user.name());
+        var lastName = new LastName(user.lastName());
+        var email = new Email(user.email());
+
+        var entity = new User(
+                name,
+                lastName,
+                email,
+                com.ssilvadev.event.api.model.user.Gender.valueOf(user.gender().name()));
+
+        return entity;
     }
 }

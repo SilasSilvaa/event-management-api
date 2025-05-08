a1ad673dd6fb5776ac3a9d06cec8198117c538e1
Silassilvaa <silas-oli@hotmail.com>
2025-05-07 22:48:59 -0300

    refactor: adding json propertiy for lastname

--- src/main/java/com/ssilvadev/event/api/dto/user/response/ResponseUserDTO.java
+++ src/main/java/com/ssilvadev/event/api/dto/user/response/ResponseUserDTO.java
@@ -1,9 +1,11 @@
 package com.ssilvadev.event.api.dto.user.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record ResponseUserDTO(
         Long id,
         String name,
-        String lastName,
+        @JsonProperty("last_name") String lastName,
         String email,
         Gender gender) {
 }

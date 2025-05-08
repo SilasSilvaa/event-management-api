a31ffa9ac20a7d0d91bdd322db186c49d378e520
Silassilvaa <silas-oli@hotmail.com>
2025-05-07 22:47:25 -0300

    feat: creating routes findById and delete and implementing service methods

--- src/main/java/com/ssilvadev/event/api/controller/UserController.java
+++ src/main/java/com/ssilvadev/event/api/controller/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +30,7 @@ public class UserController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ResponseUserDTO>> getAllUsers(
+    public ResponseEntity<Page<ResponseUserDTO>> getAll(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "size", defaultValue = "10") Integer size,
             @RequestParam(value = "direction", defaultValue = "asc") String direction) {
@@ -37,13 +39,26 @@ public class UserController {
 
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "name"));
 
-        var allUsers = service.getAllUsers(pageable);
+        var allUsers = service.getAll(pageable);
 
         return ResponseEntity.ok(allUsers);
     }
 
     @PostMapping
-    public ResponseEntity<ResponseUserDTO> createUser(@RequestBody(required = true) RequestUserDTO userDTO) {
-        return ResponseEntity.ok(service.createUser(userDTO));
+    public ResponseEntity<ResponseUserDTO> create(@RequestBody(required = true) RequestUserDTO userDTO) {
+        return ResponseEntity.ok(service.create(userDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseUserDTO> findById(@PathVariable(name = "id", required = true) Long id) {
+
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id", required = true) Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 }

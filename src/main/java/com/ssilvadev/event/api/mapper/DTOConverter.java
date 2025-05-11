b0f9921990ddb29a212c7b71533b2f62d57cd391
Silassilvaa <silas-oli@hotmail.com>
2025-05-11 12:16:07 -0300

    refactor: remove unncessary code

--- src/main/java/com/ssilvadev/event/api/mapper/DTOConverter.java
+++ src/main/java/com/ssilvadev/event/api/mapper/DTOConverter.java
@@ -10,27 +10,23 @@ import com.ssilvadev.event.api.model.user.User;
 public class DTOConverter {
 
     public static ResponseUserDTO userToResponseDTO(User user) {
-
-        var dto = new ResponseUserDTO(user.getId(),
+        return new ResponseUserDTO(user.getId(),
                 user.getName(),
                 user.getLastName(),
                 user.getEmail(),
                 Gender.valueOf(user.getGender()));
 
-        return dto;
     }
 
     public static User responseDTOToUser(ResponseUserDTO user) {
         var name = new Name(user.name());
         var lastName = new LastName(user.lastName());
         var email = new Email(user.email());
-
-        var entity = new User(
+        var gender = com.ssilvadev.event.api.model.user.Gender.valueOf(user.gender().name());
+        return new User(
                 name,
                 lastName,
                 email,
-                com.ssilvadev.event.api.model.user.Gender.valueOf(user.gender().name()));
-
-        return entity;
+                gender);
     }
 }
